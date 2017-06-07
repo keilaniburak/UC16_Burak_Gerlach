57be852bf05ebc06af0e54275b08d06e501231cc
Carolyn GERLACH <EllaGerlach@Carolyns-MacBook-Air.local>
2017-06-07 12:36:20 -0700

    updated js and html

--- uc16.js
+++ uc16.js
@@ -1,51 +1,33 @@
-/**
- * Uses AJAX to query an internet data source for zip codes
- * @param {string} zipId The element id that has the zip code
- */
-
-function findZip(zipId) {
-    // First get the zip code from the HTML textbox
-    var zip = document.getElementById(zipId).value;
-    // Now make a HTTP request
+function exchangeRate(currencyId) {
     var httpRequest = new XMLHttpRequest();
     httpRequest.onreadystatechange = function () {
         if (this.readyState === 4) {
-            // We got a response from the server!
-            if(this.status === 200) {
+            //we got a response from the server
+            if (this.status === 200) {
                 // The request was successful!
-                displayPlace(this.responseText);
-            } else if (this.status === 404){
-                // No postal code found
-                displayPlace('{ "country" : "none" }');
-            } else {
+                displayMoney(this.responseText);
+    }
+            else if (this.status === 404) {
+                displayMoney('{"currency" : "none"}');
+            }
+             else {
                 console.log("We have a problem...server responded with code: " + this.status);
             }
-        } else {
+        }
+        else {
             // Waiting for a response...
         }
     };
-    // Notice how the URL is appended with the zip code
-    var url = "http://api.zippopotam.us/us/" + zip;
+
+    var url = "http://api.fixer.io/latest?base=USD";
     httpRequest.open("GET", url, true);
     httpRequest.send();
+
 }
 
-/**
- * Displays the zip code place given the JSON data
- * @param {string} data JSON data representing place for given zip code
- */
-
-function displayPlace(data){
-    var place = JSON.parse(data);
-    if(place.country === "none") {
-        document.getElementById("place").className = "alert alert-warning";
-        document.getElementById("place").innerHTML = "No place matches that zip code."
-    } else {
-        document.getElementById("place").className = "alert alert-success";
-        document.getElementById("place").innerHTML = place.places[0]["place name"] +
-        ", " +
-        place.places[0].state +
-        ", " +
-        place.country;
+function displayMoney(data) {
+    var money = JSON.parse(data);
+    var conversionRate = USD.rates. + money;
+    var dollarAmt = document.getElementById('currency').value;
+    document.getElementById("money").innerHTML = dollarAmt * conversionRate;
     }
-}
